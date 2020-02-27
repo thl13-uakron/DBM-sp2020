b74aef68cfe6c4e08859b01d14b9c2c36491b210
thl13-uakron <thl13@zips.uakron.edu>
2020-02-27 16:13:52 -0500

    Add basic styling

--- ezChat-test/index.php
+++ ezChat-test/index.php
@@ -1,25 +1,62 @@
-<!--
-<?php 
-$ip = $_SERVER["SERVER_ADDR"];
-echo "Server address: $ip";
-?>
--->
+<title>
+	Welcome to ezChat!
+</title>
 
 <style>
 	body {
 		display: flex;
 		flex-direction: row;
+		background-color: black;
+		color: darkgray;
+		font-family: arial;
+		line-height: 1.5;
+		margin:0;
+	}
+
+	.region {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+	}
+
+	.sideRegion {
+		flex-shrink: 0.5;
+		padding: 2%;
+	}
+
+	.padded {
+		padding: 2%;
+	}
+
+	#centerRegion {
+		flex-grow: 5;
+		flex-shrink: 4;
+		background-color: lightgray;
+		color: black;
 	}
 </style>
 
+<script>
+	// functions for dynamically generating page content
+
+	function showAccountInfo() {
+		var parent = document.getElementById("accountInfo");
+		parent.innerHTML = "Account Info";
+	}
+
+</script>
+
 <body>
-	<div id="leftRegion" class="region sideRegion">
-		<div id="dmList">
-			DMs
+	<div id="leftRegion" class="region padded">
+		<div id="roomInfo">
+
 		</div>
 		<div id="roomList">
+			<div id="dmList">
+				Your DMs
+			</div>
 			<div id="recentRooms">
-				Recent Rooms
+				Your Recent Rooms
 			</div>
 			<div id="allRooms">
 				Public Rooms
@@ -27,20 +64,23 @@ echo "Server address: $ip";
 		</div>
 	</div>
 
-	<div id="centerRegion" class="region centerRegion">
-		<div id="chatRegion">
-			<div id="messageStream">
+	<div id="centerRegion" class="region">
+		<div id="chatRegion" class="region">
+			<div id="messageStream" class="padded" style="flex-grow: 5">
 				Messages
 			</div>
-			<div id="chatBox">
-				Message Box
+			<div id="chatBox" class="padded" style="flex-grow: 1; display: flex">
+				<textarea style="flex-grow: 1"></textarea>
 			</div>
 		</div>
 	</div>
 
-	<div id="rightRegion" class="region sideRegion">
-		<div id="userList">
-			Users
+	<div id="rightRegion" class="region padded">
+		<div id="accountInfo">
+			<script> showAccountInfo(); </script>
+		</div>
+		<div id="userList" style="flex-grow: 2">
+			Other Users
 		</div>
 	</div>
 </body>
