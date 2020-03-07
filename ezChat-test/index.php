6a5d6bd7c2ff171fb6aa5309a9ecf2dab97b65d2
thl13-uakron <thl13@zips.uakron.edu>
2020-03-06 19:58:25 -0500

    Add code to read and store user credentials, roomID, and channelID on client side

--- ezChat-test/index.php
+++ ezChat-test/index.php
@@ -1,10 +1,15 @@
 <?php
+	/*
 	$db_host = "sql200.main-hosting.eu";
 	$db_name = "u668413793_ezChat";
 	$db_user = "u668413793_ezChatDev";
 	$db_password = "ezChatDev";
 
 	$db = new mysqli($db_host, $db_user, $db_password, $db_name);
+	*/
+
+	include "PHP/dbAccess.php";
+	$db = get_db_connection();
 ?>
 
 <title>
@@ -28,13 +33,21 @@
 		flex-grow: 1;
 	}
 
-	.sideRegion {
-		flex-shrink: 0.5;
+	.padded {
 		padding: 2%;
 	}
 
-	.padded {
-		padding: 2%;
+	.bottomMargin {
+		margin-bottom: 2%;
+	}
+
+	#messageStream, #roomList, #userList {
+		overflow-y: auto;
+	}
+
+	#chatBox {
+		display: flex;
+		background-color: darkgray;
 	}
 
 	#centerRegion {
@@ -46,11 +59,11 @@
 </style>
 
 <body>
-	<div id="leftRegion" class="region padded">
-		<div id="roomInfo">
+	<div id="leftRegion" class="region">
+		<div id="roomInfo" class="padded">
 		</div>
 
-		<div id="roomList">
+		<div id="roomList" class="padded">
 		</div>
 	</div>
 
@@ -58,56 +71,72 @@
 		
 	</div>
 
-	<div id="rightRegion" class="region padded">
-		<div id="accountInfo">
+	<div id="rightRegion" class="region">
+		<div id="accountInfo" class="padded">
 		</div>
 
-		<div id="userList" style="flex-grow: 2">
+		<div id="userList" class="padded" style="flex-grow: 2">
 		</div>
 	</div>
 </body>
 
 <script>
-	// client parameters
-	var userID;
+	// functions for retrieving data from backend
 
 	// functions for dynamically generating page content
-	function showRoomInfo() {
+	function showRoomInfo(roomID) {
 		var parent = document.getElementById("roomInfo");
 		parent.innerHTML = 
 		`
-		
 		`;
 	}
-	function showRoomList() {
+
+	function showDMs(userID, password) {
+		var parent = document.getElementById("dmList");
+	}
+	function showRecentRooms(userID, password) {
+		var parent = document.getElementById("recentRooms");
+	}
+	function showPublicRooms(userID, password) {
+		var parent = document.getElementById("allRooms");
+		fetch("PHP/getRoomList.php", {
+			method: "POST",
+		})
+		.then();
+	}
+	function showRoomLists(userID, password) {
 		var parent = document.getElementById("roomList");
 		parent.innerHTML = 
 		`
 		<div id="dmList">
-			Your DMs
 		</div>
 		<div id="recentRooms">
-			Your Recent Rooms
 		</div>
 		<div id="allRooms">
-			Public Rooms
 		</div>
 		`;
+		showDMs(userID, password);
+		showRecentRooms(userID, password);
+		showPublicRooms(userID, password);
 	}
-	function showAccountInfo() {
+
+	function showAccountInfo(userID, password) {
 		var parent = document.getElementById("accountInfo");
-		parent.innerHTML = 
-		`
-		Account Info
-		`;
+		/*
+		fetch ("PHP/", {
+			method: "POST", 
+			body: JSON.stringify({})
+		})
+		.then()
+		*/
 	}
-	function showChat() {
+
+	function showChat(userID, password, channelID) {
 		var parent = document.getElementById("centerRegion");
 		parent.innerHTML = 
 		`
 		<div id="chatRegion" class="region">
 			<div id="messageStream" class="padded" style="flex-grow: 5">
-				Messages
 			</div>
 			<div id="chatBox" class="padded" style="flex-grow: 1; display: flex">
 				<textarea style="flex-grow: 1"></textarea>
@@ -115,25 +144,142 @@
 		</div>
 		`;
 	}
-	function showUserList() {
+
+	function showUserLists(userID, password) {
 		var parent = document.getElementById("userList");
 		parent.innerHTML =
 		`
-		Other Users
 		`;
 	}
 
-	function showPage() {
-		showRoomInfo();
-		showRoomList();
-		showChat();
-		showAccountInfo();
-		showUserList();
+	function showPage(userID, password, channelID, roomID) {
+		showRoomInfo(userID, password, roomID);
+		showRoomLists(userID, password);
+		showChat(userID, password, channelID);
+		// showAccountInfo(username, password);
+		showUserLists(userID, password);
 	}
 
-	// functions for retrieving data from backend
+	function showLogin() {
+		parent = document.getElementById("loginPanel");
+		parent.innerHTML = `
+		<div> <b> Login </b> </div>
+		<div> Username: <input id='usernameInput'> </input> </div>
+		<div> Password: <input id='passwordInput'> </input> </div>
+		<div> <button id='loginCancelButton' value='Cancel' /> <button id='loginButton' value='Go' /> </div>
+		<div id='loginStatus'> </div>
+		`;
+	}
 
-	// render content
-	showPage();
-	
+	// get client parameters
+	var userID = `<?php
+		$userID = null;
+		# get userID from cookie if present
+		if (array_key_exists("userID", $_COOKIE)) {
+			$userID = $_COOKIE["userID"];
+		}
+		echo $userID;
+	?>`;
+
+	var password = `<?php
+		$password = null;
+		# get password from cookie if present
+		if (array_key_exists("password", $_COOKIE)) {
+			$password = $_COOKIE["password"];
+		}
+		echo $password;
+	?>`;
+
+	var channelID = `<?php
+		function isValidChannel($db, $channelID) {
+			$queryResult = $db->query("call getChannelInfo('$channelID')");
+			return count($queryResult->fetch_all()) > 0;
+		}
+		$channelID = "null";
+		# get channel ID from GET parameter
+		if (array_key_exists("channelID", $_GET)) {
+			$channelID = $_GET["channelID"];
+			# check if ID is valid
+			if (!isValidChannel($db, $channelID)) {
+				$channelID = "null";
+			}
+		}
+		# get channel ID from cookie if GET parameter not present or invalid
+		if (!$channelID && (array_key_exists("channelID", $_COOKIE))) {
+			# reset db connection
+			$db->close();
+			$db = get_db_connection();
+
+			$channelID = $_COOKIE["channelID"];
+			if (!isValidChannel($db, $channelID)) {
+				$channelID = "null";
+			}
+		}
+		echo $channelID;
+	?>`;
+
+	var roomID = `<?php
+		function isValidRoom($db, $roomID) {
+			$queryResult = $db->query("call getRoomInfo('$roomID')");
+			return count($queryResult->fetch_all()) > 0;
+		}
+		# reset db connection
+		$db->close();
+		$db = get_db_connection();
+
+		$roomID = "null";
+		# get room ID from channel ID is present
+		if ($channelID) {
+			$queryResult = $db->query("call getChannelInfo('$channelID')");
+			if ($queryResult) {
+	 			$roomID = $queryResult->fetch_assoc()["roomID"];
+			}
+		}
+		else {
+			# get from GET parameter otherwise (leave null if no room ID associated with channel)
+			if (array_key_exists("roomID", $_GET)) {
+				$roomID = $_GET["roomID"];
+				# check if ID is valid
+				if (!isValidRoom($db, $roomID)) {
+					$channelID = "null";
+				}
+			}
+			# set to default room if both roomID and channelID are null
+			else {
+				$roomID = 2;
+			}
+		}
+
+		# reset db connection
+		$db->close();
+		$db = get_db_connection();
+
+		echo $roomID;
+	?>`;
+
+	// assign guest ID if current ID/password not valid
+	if (!Number(`<?php echo ($userID != null && $db->query("select validateUser('$userID', '$password', true)")->fetch_array()[0]) ?>`)) {
+		fetch("PHP/addUser.php", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+			body: JSON.stringify({
+				"screenName": "guest"
+			})
+		})
+		.then((response) => response.json())
+		.then((data) => {
+			userID = data["userID"];
+			document.cookie = "userID=" + userID;
+			showPage(userID, password, channelID, roomID);
+		})
+		.catch((error) => console.log(error))
+	}
+	else {
+		// render page content otherwise
+		showPage(userID, password, channelID, roomID);
+	}
+
 </script>
